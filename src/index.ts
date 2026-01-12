79c42349bcf3e7245d6a638f13e5a84723edffd7
Nick Latkovich <nicklatkovich@gmail.com>
2026-01-12 20:56:52 +0300

    Rename "empty" to "nil" && Set serializer

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ export { BigNumber } from "bignumber.js";
 
 export { default as bool } from "./bool";
 export { default as bytes, Encoding } from "./bytes";
-export { default as empty } from "./empty";
+export { default as nil } from "./nil";
 export { default as ISerializer, BaseOf, InputOf, JSONOf } from "./ISerializer";
 export { default as optional, OptionalSerializer } from "./optional";
 export { default as chars } from "./chars";
+export { default as set, SetSerializer } from "./set";
 export { default as struct, StructSerializer, extended } from "./struct";
 export { default as varuint } from "./varuint";
 export { default as vector, VectorSerializer } from "./vector";
