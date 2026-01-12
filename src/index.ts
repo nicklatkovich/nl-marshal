db7250055c0a71d8a2d5eaf7109cc38b0440db67
Nick Latkovich <nicklatkovich@gmail.com>
2026-01-12 20:56:52 +0300

    0.2.0-rc.4

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ export { default as empty } from "./empty";
 export { default as ISerializer, BaseOf, InputOf, JSONOf } from "./ISerializer";
 export { default as optional, OptionalSerializer } from "./optional";
 export { default as chars } from "./chars";
-export { default as struct, StructSerializer } from "./struct";
+export { default as struct, StructSerializer, extended } from "./struct";
 export { default as varuint } from "./varuint";
 export { default as vector, VectorSerializer } from "./vector";
